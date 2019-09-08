9079e1248a3f37c7e9de3c8a3324d283650e78fa
Kirill Borisov <kirill-borisov@yandex.ru>
2019-09-08 23:11:32 +0300

    test 2
    test 2

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const ansi  = require("ansi");
-const faker = require("faker");
+const ansi   = require("ansi");
+const faker  = require("faker");
 
-console.log("Hello, " + faker.internet.userName() + "!");
+const cursor = ansi(process.stdout);
+
+cursor.write("Wake up, ").red().bg.yellow().write(faker.internet.userName()).reset().bg.reset().write(" ...\n");
+cursor.green().write("Follow the white rabbit ...").reset().write("\n");
